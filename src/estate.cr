5cd18aa0c157245d167cbfa2926659b98f8a783c
stepanvanzuriak <stepanvanzuriak@gmail.com>
2018-08-24 12:15:38 +0300

    v0.1.0

---
+++ src/estate.cr
@@ -0,0 +1,54 @@
+# TODO: Write documentation for `Estate`
+module Estate
+  extend self
+
+  VERSION = "0.1.0"
+
+  def create(initial)
+    State.new(initial)
+  end
+  
+  class State(T)
+    def initialize(initial : T)
+      @state = initial
+    end
+
+    def get
+      @state
+    end
+
+    def get(selector : Tuple)
+      if (selector.size == 1)
+        @state[selector.first]
+      else
+        selector.map { |name| @state[name] }
+      end
+    end
+
+    def set
+      @state
+    end
+
+    def set(&block)
+      @state = @state.merge(yield @state)
+    end
+
+    def select
+      @state
+    end
+
+    def select(selector : Tuple)
+      if (selector.size == 1)
+       -> { @state[selector.first] }
+      else
+       -> { selector.map { |name| @state[name] } }
+      end
+    end
+
+    def select(&block)
+      selected = yield @state
+      -> {selected}
+    end
+
+  end
+end
